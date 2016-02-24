b57b377acf77759c28ecf737f0fc15149be35d36
Sven de Windt <sven.dewindt.d2579@student.hogent.be>
2016-02-24 21:52:52 +0100

    powershell met logging

--- MasterSmall/CustomScripts/startOrchestration.ps1
+++ MasterSmall/CustomScripts/startOrchestration.ps1
@@ -1,4 +1,8 @@
 <# Custom Script for Windows #>
+
+
+
+
 param(
         [parameter(mandatory)][String]$RDSBroker,
         [parameter(mandatory)][String]$RDSWebAccess,
@@ -7,20 +11,60 @@ param(
 
     #$VerbosePreference = "Continue"
 
+function new-logFile{
+param(
+    [parameter(mandatory)][String]$log
+)
+    if (Test-Path $log){
+        Remove-Item $log -Force
+    }
+    New-Item -Path $log -ItemType file
+}
+
+function write-log{
+param(
+    #[String]$log = "c:\azureSetup.log",
+    [String]$line,
+    [String]$title
+)
+    $time = Get-Date
+    if ($line){
+        Add-Content -Path $log -Value "$($time) - $($line)"
+    }
+
+    if ($title){
+        Add-Content -Path $log -Value ""
+        Add-Content -Path $log -Value "$($time) ***** $($title) *****"
+    }
+    
+}
+    
+    $log = "c:\azureSetup.log"
+    new-logFile -log $log
+
     $fqdnRDSLicensing = [System.Net.Dns]::GetHostByName($RDSLicensing).hostname
     $fqdnRDSBroker = [System.Net.Dns]::GetHostByName($RDSBroker).hostname
     $fqdnRDSWebAccess = [System.Net.Dns]::GetHostByName($RDSWebAccess).hostname
     $fqdnRDSHost = [System.Net.Dns]::GetHostByName($RDSHost).hostname
 
-    Write-Verbose "FQDN Broker     : $($fqdnRDSBroker)"
-    Write-Verbose "FQDN WebAccess  : $($fqdnRDSWebAccess)"
-    Write-Verbose "FQDN RDS        : $($fqdnRDSHost)"
+    write-log -title "Starting Orchestration"
+    write-log -line "titleariables used:"
 
-    Write-Verbose "Setting up SessionDeployment"
-    New-RDSessionDeployment -ConnectionBroker $fqdnRDSBroker -WebAccessServer $fqdnRDSWebAccess -SessionHost $fqdnRDSHost
+    write-log -line "FQDN Broker     : $($fqdnRDSBroker)"
+    write-log -line "FQDN WebAccess  : $($fqdnRDSWebAccess)"
+    write-log -line "FQDN RDS        : $($fqdnRDSHost)"
 
-    Write-Verbose "Adding License server"
-    Add-RDServer -Server $fqdnRDSBroker -Role RDS-LICENSING -ConnectionBroker $fqdnRDSBroker
 
-    Write-Verbose "Configuring Licensing mode per user"
-    Set-RDLicenseConfiguration -LicenseServer $fqdnRDSBroker -Mode PerUser -ConnectionBroker $fqdnRDSBroker -Force
+    
+    write-log -title "lineSetting up SessionDeploymentline"
+    $output = New-RDSessionDeployment -ConnectionBroker $fqdnRDSBroker -WebAccessServer $fqdnRDSWebAccess -SessionHost $fqdnRDSHost 2>&1
+    write-log -line $output
+
+    write-log -title "lineAdding License serverline"
+    $output = Add-RDServer -Server $fqdnRDSBroker -Role RDS-LICENSING -ConnectionBroker $fqdnRDSBroker 2>&1
+    write-log -line $output
+
+    write-log -title "lineConfiguring Licensing mode per userline"
+    $output = Set-RDLicenseConfiguration -LicenseServer $fqdnRDSBroker -Mode PerUser -ConnectionBroker $fqdnRDSBroker -Force 2>&1
+    write-log -line $output
+
