40dd74370c34c179e9241c81b637a4e693d3ed4d
Sven de Windt <sven.de.windt@gmail.com>
2016-02-26 23:17:01 +0100

    met aparte module logging en apart script install

--- MasterSmall/CustomScripts/startOrchestration.ps1
+++ MasterSmall/CustomScripts/startOrchestration.ps1
@@ -1,79 +1,30 @@
 <# Custom Script for Windows #>
-
-
-
-
 param(
         [parameter(mandatory)][String]$RDSBroker,
         [parameter(mandatory)][String]$RDSWebAccess,
-        [parameter(mandatory)][String]$RDSHost
+        [parameter(mandatory)][String]$RDSHost,
+        [parameter(mandatory)][String]$userName,
+        [parameter(mandatory)][SecureString]$password
     )
 
-    #$VerbosePreference = "Continue"
-
-function new-logFile{
-param(
-    [parameter(mandatory)][String]$log
-)
-    if (Test-Path $log){
-        Remove-Item $log -Force
-    }
-    New-Item -Path $log -ItemType file
-}
-
-function write-log{
-param(
-    #[String]$log = "c:\azureSetup.log",
-    [String]$line,
-    [String]$title
-)
-    $time = Get-Date
-    if ($line){
-        Add-Content -Path $log -Value "$($time) - $($line)"
-    }
-
-    if ($title){
-        Add-Content -Path $log -Value ""
-        Add-Content -Path $log -Value "$($time) ***** $($title) *****"
-    }
+    #Getting required modules/scripts
+    #Get Logging.psm1 in the modules folder
+    $source = "https://raw.githubusercontent.com/svendewindt/azure/master/MasterSmall/CustomScripts/Logging.psm1"
+    $destination = "C:\Program Files\WindowsPowerShell\Modules"
+    $folderName = "Logging"
+    #Create folder in the modules folder
+    New-Item -Path $destination -Name $folderName -ItemType directory
+    #Download the logging module
+    Invoke-WebRequest -Uri $source -OutFile "$($destination)\$($folderName)\Logging.psm1" 
+    #Logging functions can be used now.
     
-}
+    #Get DeployRDS.ps1
+    $source = "https://raw.githubusercontent.com/svendewindt/azure/master/MasterSmall/CustomScripts/DeployRDS.ps1"
+    $destination = $PSScriptRoot
+    $deployRDSScript = "$($PSScriptRoot)\deployRDS.ps1"
+    Invoke-WebRequest -Uri $source -OutFile "$deployRDSScript"
 
-function tryPsRemoting{
-    param(
-        [parameter(mandatory)][String]$server
-    )
-
-    write-log -title "Trying $($server)"
-    try{
-        write-log -line "New-Pssesion"
-        $output = $psSession = New-PSSession -ComputerName $server 2>&1
-        write-log -line $output
-        #write-host "computername: $($psSession.ComputerName)" 
-        if ($psSession.ComputerName.Length -gt 0){
-            write-log -line "Enter-Pssesion"
-            $output = Enter-PSSession $psSession -ErrorAction SilentlyContinue
-            write-log -line $output
-
-            write-log -line "Successful logged in on $($server)"
-            
-            write-log "Exit-PSSession"
-            $output =Exit-PSSession -ErrorAction SilentlyContinue
-            write-log -line $output
-
-            write-log -line "Closing Pssesion"
-            $output = Get-PSSession -ComputerName $server | Remove-PSSession
-            write-log -line $output
-        } else {
-            write-log "Unable to log on to $($server)"
-        }
-        
-    }catch{
-        write-log -line $_.exception
-    }
-
-}
-    
+   
     $log = "c:\azureSetup.log"
     new-logFile -log $log
 
@@ -81,41 +32,30 @@ function tryPsRemoting{
     $fqdnRDSBroker = [System.Net.Dns]::GetHostByName($RDSBroker).hostname
     $fqdnRDSWebAccess = [System.Net.Dns]::GetHostByName($RDSWebAccess).hostname
     $fqdnRDSHost = [System.Net.Dns]::GetHostByName($RDSHost).hostname
-
-    $servers = $fqdnRDSBroker, $fqdnRDSWebAccess, $fqdnRDSHost
-
-    write-log -title "Starting Orchestration"
-    write-log -line "Variables used:"
-
-    write-log -line "FQDN Broker         : $($fqdnRDSBroker)"
-    write-log -line "FQDN WebAccess      : $($fqdnRDSWebAccess)"
-    write-log -line "FQDN RDS            : $($fqdnRDSHost)"
-    $userName = [Environment]::UserName
-    $domainName = [Environment]::UserDomainName
-    $machineName = [Environment]::MachineName
-    write-log -line "Domainname\username : $($domainName)\\$($userName)" 
-    write-log -line "MachineName         : $($machineName)" 
-	write-log -line "ScriptRoot          : $($PSScriptRoot)"
-
-    #write-log -title "Sleeping for 5 minutes to let all servers boot..."
-    #Start-Sleep -Seconds 300
-
-    write-log -title "Trying to ps remote to the servers"
-    foreach ($server in $servers){
-        tryPsRemoting -server $server
-    }
     
+    write-log -log $log -title "Starting Orchestration"
+    write-log -log $log -line "Variables used:"
+
+    write-log -log $log -line "FQDN Broker         : $($fqdnRDSBroker)"
+    write-log -log $log -line "FQDN WebAccess      : $($fqdnRDSWebAccess)"
+    write-log -log $log -line "FQDN RDS            : $($fqdnRDSHost)"
     
-    write-log -title "Setting up SessionDeployment"
-    $output = New-RDSessionDeployment -ConnectionBroker $fqdnRDSBroker -WebAccessServer $fqdnRDSWebAccess -SessionHost $fqdnRDSHost 2>&1
-    write-log -line $output
+    $domainName = (gwmi WIN32_ComputerSystem).domain
+    write-log -log $log -line "Domainname\username : $($domainName)\$($userName)"
+    #$securePassword = ConvertTo-SecureString $password -AsPlainText -Force 
+    $securePassword = $password
+    $credential = New-Object System.Management.Automation.PSCredential ("$domainName\$($userName)",$securePassword)
 
-    write-log -title "Adding License server"
-    $output = Add-RDServer -Server $fqdnRDSBroker -Role RDS-LICENSING -ConnectionBroker $fqdnRDSBroker 2>&1
-    write-log -line $output
+    
+    Write-Host "Script to run: $($deployRDSScript)"
+    Invoke-Command -FilePath $deployRDSScript -ComputerName $fqdnRDSBroker -Credential $credential -ArgumentList $fqdnRDSBroker, $fqdnRDSWebAccess, $fqdnRDSHost, $log
 
-    write-log -title "Configuring Licensing mode per user"
-    $output = Set-RDLicenseConfiguration -LicenseServer $fqdnRDSBroker -Mode PerUser -ConnectionBroker $fqdnRDSBroker -Force 2>&1
-    write-log -line $output
+
+    #.\1.ps1 -RDSBroker g1dc01 -RDSWebAccess g1rdswa01 -RDSHost g1rds01 -userName "adminsvdw" -password "Enter123"
+
+
+
+
+
 
     
