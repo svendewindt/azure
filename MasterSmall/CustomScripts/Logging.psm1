633f1c27347348570c7889203dc448daf677d500
svendewindt <svendewindt@users.noreply.github.com>
2016-02-26 21:37:21 +0000

    Added files via upload

---
+++ MasterSmall/CustomScripts/Logging.psm1
@@ -0,0 +1,28 @@
+﻿function new-logFile{
+param(
+    [parameter(mandatory)][String]$log
+)
+    if (Test-Path $log){
+        Remove-Item $log -Force
+    }
+    New-Item -Path $log -ItemType file
+}
+
+function write-log{
+param(
+    #[String]$log = "c:\azureSetup.log",
+    [parameter(mandatory)][String]$log,
+    [String]$line,
+    [String]$title
+)
+    $time = Get-Date
+    if ($line){
+        Add-Content -Path $log -Value "$($time) - $($line)"
+    }
+
+    if ($title){
+        Add-Content -Path $log -Value ""
+        Add-Content -Path $log -Value "$($time) ***** $($title) *****"
+    }
+    
+}
