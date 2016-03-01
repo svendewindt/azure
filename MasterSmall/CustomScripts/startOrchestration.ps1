3ede5c5b71213fb3b40880c8d95ee3640d1f63af
svendewindt <svdw@adifo.com>
2016-03-01 10:40:42 +0100

    without passing passwords

--- MasterSmall/CustomScripts/startOrchestration.ps1
+++ MasterSmall/CustomScripts/startOrchestration.ps1
@@ -2,13 +2,12 @@
 param(
         [parameter(mandatory)][String]$RDSBroker,
         [parameter(mandatory)][String]$RDSWebAccess,
-        [parameter(mandatory)][String]$RDSHost,
-        [parameter(mandatory)][String]$userName,
-        [parameter(mandatory)][SecureString]$password
+        [parameter(mandatory)][String]$RDSHost
     )
 
     ##############
     #Testing
+    Set-StrictMode -Version 3.0
     $testLog = "c:\azureTestLog.log"
 
     New-Item -Path $testLog -ItemType file
@@ -16,11 +15,7 @@ Add-Content -Path $testLog -Value "Starting session."
 Add-Content -Path $testLog -Value "$($RDSBroker)"
 Add-Content -Path $testLog -Value "$($RDSWebAccess)"
 Add-Content -Path $testLog -Value "$($RDSHost)"
-Add-Content -Path $testLog -Value "$($userName)"
-
-    
     ##############
-
 
     #Getting required modules/scripts
     #Get Logging.psm1 in the modules folder
@@ -44,6 +39,12 @@ Add-Content -Path $testLog -Value "Output invoke-webrequest $($output)"
     $output = Import-module Logging.psm1  2>&1
 Add-Content -Path $testLog -Value "Output import-module $($output)"
 	#Logging functions can be used now.
+
+    #Creating Installer user
+    $username = "Installer"
+    $securePassword = ConvertTo-SecureString "Enter123" -AsPlainText -Force 
+    New-ADUser -Name $username -SamAccountName $username -UserPrincipalName $username -AccountPassword $securePassword -Enabled $true
+    Add-ADGroupMember "domain admins" -Members $username 
     
     #Get DeployRDS.ps1
     $source = "https://raw.githubusercontent.com/svendewindt/azure/master/MasterSmall/CustomScripts/DeployRDS.ps1"
@@ -55,7 +56,6 @@ Add-Content -Path $testLog -Value "Output import-module $($output)"
     $log = "c:\azureSetup.log"
     new-logFile -log $log
 
-    $fqdnRDSLicensing = [System.Net.Dns]::GetHostByName($RDSLicensing).hostname
     $fqdnRDSBroker = [System.Net.Dns]::GetHostByName($RDSBroker).hostname
     $fqdnRDSWebAccess = [System.Net.Dns]::GetHostByName($RDSWebAccess).hostname
     $fqdnRDSHost = [System.Net.Dns]::GetHostByName($RDSHost).hostname
@@ -66,17 +66,15 @@ Add-Content -Path $testLog -Value "Output import-module $($output)"
     write-log -log $log -line "FQDN Broker         : $($fqdnRDSBroker)"
     write-log -log $log -line "FQDN WebAccess      : $($fqdnRDSWebAccess)"
     write-log -log $log -line "FQDN RDS            : $($fqdnRDSHost)"
+
     
     $domainName = (gwmi WIN32_ComputerSystem).domain
     write-log -log $log -line "Domainname\username : $($domainName)\$($userName)"
-    #$securePassword = ConvertTo-SecureString $password -AsPlainText -Force 
-    $securePassword = $password
     $credential = New-Object System.Management.Automation.PSCredential ("$domainName\$($userName)",$securePassword)
-
     
     Write-Host "Script to run: $($deployRDSScript)"
     Invoke-Command -FilePath $deployRDSScript -ComputerName $fqdnRDSBroker -Credential $credential -ArgumentList $fqdnRDSBroker, $fqdnRDSWebAccess, $fqdnRDSHost, $log
 
 
-    #.\1.ps1 -RDSBroker g1dc01 -RDSWebAccess g1rdswa01 -RDSHost g1rds01 -userName "adminsvdw" -password "Enter123"
+    #.\1.ps1 -RDSBroker g1dc01 -RDSWebAccess g1rdswa01 -RDSHost g1rds01
 
