0b751debddd34d45e97d9d892a229aa8774efebb
svendewindt <svdw@adifo.com>
2016-09-26 12:16:09 +0200

    opnieuw

--- MasterAxTest/AzureDeploy/DSC/configureSQL.ps1
+++ MasterAxTest/AzureDeploy/DSC/configureSQL.ps1
@@ -2,31 +2,19 @@
 {
 
     param(
-        [String]$nodeName = "localhost",
-        [Parameter(mandatory)][String]$domainName,
-        [Parameter(mandatory)][System.Management.Automation.PSCredential]$Admincreds
-
+        [String]$nodeName = "localhost"
     )
 
-    Import-DscResource -moduleName cdisk, xdisk, PSDesiredStateConfiguration, xComputerManagement, xPendingReboot
+    Import-DscResource -moduleName cdisk, xdisk, PSDesiredStateConfiguration
 
     Node $nodeName{
 
-        xPendingReboot checkReboot{
-            name = "CheckReboot"
-        }
 
         LocalConfigurationManager
         {
             RebootNodeIfNeeded = $true
         }
 
-        xComputer JoinDomain
-        {
-            Name = $env:COMPUTERNAME
-            DomainName = $domainName
-            Credential = $domaincreds
-        }
 
         xWaitforDisk Disk2{
             DiskNumber = 2
@@ -37,46 +25,7 @@
         cDiskNoRestart FData{
             DiskNumber = 2
             DriveLetter = 'F'
-        }
-
-
-        WindowsFeature RDS-RD-Server
-        {
-            Ensure = "Present"
-            Name = "RDS-RD-Server"
-            DependsOn = "[xComputer]JoinDomain" 
-        }
-
-        WindowsFeature RSAT-RDS-Licensing-Diagnosis-UI
-        {
-            Ensure = "Present"
-            Name = "RSAT-RDS-Licensing-Diagnosis-UI"
-            DependsOn = "[xComputer]JoinDomain" 
-        }
-
-        WindowsFeature desktop-experience
-        {
-            Ensure = "Present"
-            Name = "desktop-experience"
-            DependsOn = "[xComputer]JoinDomain" 
-        }
-
-                WindowsFeature RDS-Web-Access
-        {
-            Ensure = "Present"
-            Name = "RDS-Web-Access"
-        }
-
-        WindowsFeature RDS-Gateway
-        {
-            Ensure = "Present"
-            Name = "RDS-Gateway"
-        }
         
-        WindowsFeature RSAT-RDS-Gateway
-        {
-            Ensure = "Present"
-            Name = "RSAT-RDS-Gateway"
         }
 
 
